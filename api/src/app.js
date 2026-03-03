961298971ea0fa65e73d819708302d343ea6b3d3
Arnav8998 <arnavmj5@gmail.com>
2026-03-03 04:36:55 +0000

    Set up Express app with ticket routes

--- api/src/app.js
+++ api/src/app.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const ticketsRoutes = require("./routes/tickets.routes");
 
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "IT Support Ticketing API running" });
+});
+
+// API routes
+app.use("/api/tickets", ticketsRoutes);
+
+module.exports = app;
